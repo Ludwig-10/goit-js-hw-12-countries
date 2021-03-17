6a4c22109661063ccd4c0487935a12134aa70ad1
Denys Karakoi <denys.karakoi@gmail.com>
2021-03-17 01:36:38 +0100

    Updates

--- src/settings/refs.js
+++
@@ -1,4 +0,0 @@
-  const   searchContainer = document.querySelector('.search-country-container');
-   const  searchInput = document.querySelector('.input');
-   const searchList = document.querySelector('.search-country-list-result');
-  export {searchContainer, searchInput, searchList};
